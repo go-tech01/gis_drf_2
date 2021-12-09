3cfc67324d3f49a4287a833b13ce263bc0645b50
MSI-HAK\pc <kyh12kr@naver.com>
2021-12-09 13:24:42 +0900

    accountapp urls

--- accountapp/views.py
+++ accountapp/views.py
@@ -1,5 +1,9 @@
 from django.shortcuts import render
 
 # Create your views here.
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
 
-# def hello_world(request):
+@api_view()
+def hello_world(request):
+    return Response({'message':'Hello world!!'})
