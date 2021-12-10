b3002d51d1e392a5a2ff1b104d36660bf0cd7586
MSI-HAK\pc <kyh12kr@naver.com>
2021-12-10 10:26:35 +0900

    development static file serving(js)

--- accountapp/urls.py
+++ accountapp/urls.py
@@ -1,3 +1,5 @@
+from django.conf import settings
+from django.conf.urls.static import static
 from django.urls import path
 from accountapp.views import hello_world, hello_world_template
 
@@ -6,4 +8,4 @@ app_name = 'accountapp'
 urlpatterns = [
     path('hello_world_template/', hello_world_template, name='hello_world_template'),
     path('hello_world/', hello_world, name='hello_world'),
-]
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
