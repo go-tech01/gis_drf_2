870400e63db9f554c7389c705c45b7924bd18dd4
MSI-HAK\pc <kyh12kr@naver.com>
2021-12-10 10:56:42 +0900

    templates design

--- accountapp/urls.py
+++ accountapp/urls.py
@@ -4,6 +4,8 @@ from accountapp.views import hello_world, hello_world_template
 app_name = 'accountapp'
 
 urlpatterns = [
+    # UI를 보기 위한 부분
     path('hello_world_template/', hello_world_template, name='hello_world_template'),
+    # 로직 처리 위한 부분
     path('hello_world/', hello_world, name='hello_world'),
 ]
