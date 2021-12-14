be186381a63b3fb0f91624f7bce46636340893f7
MSI-HAK\pc <kyh12kr@naver.com>
2021-12-14 15:10:32 +0900

    update template error code

--- accountapp/static/js/update.js
+++ accountapp/static/js/update.js
@@ -28,8 +28,15 @@ function update_account(pk) {
         .catch(function (error) {
             // handle error
             console.log(error);
+            if (error.response.status === 401){
             document.getElementById('alert_box').innerHTML
-                    = "<div class='btn btn-danger rounded-pill px-5'>업데이트에 실패했습니다.</div>"
+                    = "<div class='btn btn-danger rounded-pill px-5'>인증 정보가 없습니다.</div>"}
+            else if (error.response.status === 403){
+            document.getElementById('alert_box').innerHTML
+                    = "<div class='btn btn-danger rounded-pill px-5'>권한이 없습니다.</div>"}
+            else{
+            document.getElementById('alert_box').innerHTML
+                    = "<div class='btn btn-danger rounded-pill px-5'>업데이트에 실패했습니다.</div>"}
         })
         .then(function () {
             // always executed
