b0073414d420d3423670824b57965d3a12096109
MSI-HAK\pc <kyh12kr@naver.com>
2021-12-14 10:37:17 +0900

    Update View Template only html

--- accountapp/urls.py
+++ accountapp/urls.py
@@ -3,7 +3,8 @@ from django.views.generic import TemplateView
 from rest_framework.authtoken import views
 
 from accountapp.views import hello_world, hello_world_template, AccountCreateTemplate, AccountCreateAPIView, \
-    AccountLoginView, AccountRetrieveAPIView, AccountRetrieveTemplateView, AccountUpdateAPIView
+    AccountLoginView, AccountRetrieveAPIView, AccountRetrieveTemplateView, AccountUpdateAPIView, \
+    AccountUpdateTemplateView
 
 app_name = 'accountapp'
 
@@ -31,5 +32,6 @@ urlpatterns = [
     path('retrieve/<int:pk>', AccountRetrieveAPIView.as_view(), name='retrieve'),
     # path('list/', ListUsers.as_view(), name='list'),
 
+    path('update_template/<int:pk>', AccountUpdateTemplateView.as_view(), name='update_template'),
     path('update/<int:pk>', AccountUpdateAPIView.as_view(), name='update'),
 ]
