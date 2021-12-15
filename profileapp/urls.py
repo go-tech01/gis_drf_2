bb4d14a543ae3b10c9a7c17a45416b56a4146038
MSI-HAK\pc <kyh12kr@naver.com>
2021-12-15 14:50:02 +0900

    Profile Create Template html

--- profileapp/urls.py
+++ profileapp/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path
-from profileapp.views import ProfileCreateAPIView
+from profileapp.views import ProfileCreateAPIView, ProfileCreateTemplateView
+
+app_name = 'profileapp'
 
 urlpatterns = [
     path('', ProfileCreateAPIView.as_view(), name='create'),
+    path('create_template/', ProfileCreateTemplateView.as_view(), name='create_template'),
 ]
--- profileapp/views.py
+++ profileapp/views.py
@@ -1,9 +1,13 @@
 from django.shortcuts import render
+from django.views.generic import TemplateView
 from rest_framework import permissions
 from rest_framework.authentication import TokenAuthentication
 from rest_framework.generics import CreateAPIView
 from profileapp.models import Profile
 from profileapp.serializers import ProfileSerializer
+
+class ProfileCreateTemplateView(TemplateView):
+    template_name = 'profileapp/create.html'
 
 class ProfileCreateAPIView(CreateAPIView):
     queryset = Profile.objects.all()
