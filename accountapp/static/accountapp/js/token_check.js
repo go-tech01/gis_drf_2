23db47c9e88f272d1df4c16344e7b5023b0178e1
MSI-HAK\pc <kyh12kr@naver.com>
2021-12-16 11:55:20 +0900

    header nav button mypage

--- accountapp/static/accountapp/js/token_check.js
+++ accountapp/static/accountapp/js/token_check.js
@@ -5,21 +5,26 @@ function getCookie(name) {
   return matches ? decodeURIComponent(matches[1]) : undefined;
 }
 
-var check = getCookie('drf_token');
-
-// get 기반 token 주인의 pk 획득
-
+var user_id;
 axios({
     method: 'get',
-    url: '/accounts/',
-
+    url: '/accounts/token/',
+    headers:{
+        Authorization: decodeURIComponent(getCookie('drf_token'))
+    }
 })
-
-if (check !== undefined) {
-    document.getElementById('signup_button').innerHTML =
-        "<a href=\"/accounts/retrieve_template/\">\n" +
+    .then(function (response){
+        // 성공했을 경우
+        user_id = response.data['id'];
+        document.getElementById('signup_button').innerHTML =
+        "<a href=\"/accounts/retrieve_template/" + user_id + "\">\n" +
         "                MyPage\n" +
         "            </a>";
+    })
+
+var check = getCookie('drf_token');
+
+if (check !== undefined) {
     document.getElementById('login_button').innerHTML =
         "<a href=\"/accounts/logout_template/\">\n" +
         "                Logout\n" +
