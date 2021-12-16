31450789ad361b3d74cc2099cc5c51f4f1e1a9f4
MSI-HAK\pc <kyh12kr@naver.com>
2021-12-16 13:39:24 +0900

    media url / accountapp serializer

--- accountapp/serializes.py
+++ accountapp/serializes.py
@@ -1,6 +1,9 @@
 from django.contrib.auth.models import User
 from rest_framework import serializers
 
+from profileapp.serializers import ProfileSerializer
+
+
 class NewModelSerializer(serializers.Serializer):
     text = serializers.CharField(max_length=255)
     created_at = serializers.DateTimeField()
@@ -15,6 +18,7 @@ class UserSerializer(serializers.ModelSerializer):      # form
         return user
 
 class UserWithoutPasswordSerializer(serializers.ModelSerializer):
+    profile =ProfileSerializer(read_only=True)
     class Meta:
         model = User
-        fields = ['id','username', 'email', 'date_joined']
+        fields = ['id','username', 'email', 'date_joined','profile']
--- accountapp/static/accountapp/js/retrieve.js
+++ accountapp/static/accountapp/js/retrieve.js
@@ -3,9 +3,9 @@ function initialize(pk) {
         .then(function (response) {
             // handle success
             console.log(response);
-            document.getElementById('image').src = response.data['image'];
-            document.getElementById('nickname').innerHTML = response.data['nickname'];
-            document.getElementById('message').innerHTML = response.data['message'];
+            document.getElementById('image').src = response.data['profile']['image'];
+            document.getElementById('nickname').innerHTML = response.data['profile']['nickname'];
+            document.getElementById('message').innerHTML = response.data['profile']['message'];
         })
         .catch(function (error) {
             // handle error
