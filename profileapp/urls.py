cf52c7d5c1821826c6c21bf4e7a61d32ea0010aa
MSI-HAK\pc <kyh12kr@naver.com>
2021-12-16 10:28:21 +0900

    Profile Update Template js auth RetrieveUpdateView

--- profileapp/urls.py
+++ profileapp/urls.py
@@ -1,12 +1,13 @@
 from django.urls import path
-from profileapp.views import ProfileCreateAPIView, ProfileCreateTemplateView, ProfileUpdateAPIView, \
+from profileapp.views import ProfileCreateAPIView, ProfileCreateTemplateView, ProfileRetrieveUpdateAPIView, \
     ProfileUpdateTemplateView
 
 app_name = 'profileapp'
 
 urlpatterns = [
-    path('', ProfileCreateAPIView.as_view(), name='create'),
-    path('<int:pk>', ProfileUpdateAPIView.as_view(), name='update'),
     path('create_template/', ProfileCreateTemplateView.as_view(), name='create_template'),
     path('update_template/<int:pk>', ProfileUpdateTemplateView.as_view(), name='update_template'),
+
+    path('', ProfileCreateAPIView.as_view(), name='create'),
+    path('<int:pk>', ProfileRetrieveUpdateAPIView.as_view(), name='update'),
 ]
