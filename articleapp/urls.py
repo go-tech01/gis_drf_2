f61196f83c1148383c6b46dc671cef1b0a77a432
MSI-HAK\pc <kyh12kr@naver.com>
2021-12-17 11:44:24 +0900

    article RUD api view & permission

--- articleapp/urls.py
+++ articleapp/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from articleapp.views import ArticleCreateAPIView, ArticleTemplateView
+from articleapp.views import ArticleCreateAPIView, ArticleTemplateView, ArticleRUDAPIView
 
 app_name = 'articleapp'
 
 urlpatterns = [
     path('create_template/', ArticleTemplateView.as_view(), name='create_template'),
     path('', ArticleCreateAPIView.as_view(), name='create'),
+    path('<int:pk>', ArticleRUDAPIView.as_view(), name='RUD'),
 ]
