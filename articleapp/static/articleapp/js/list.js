1d7fd432af4de76916462d7d83b43c9b3b575161
MSI-HAK\pc <kyh12kr@naver.com>
2021-12-22 13:45:39 +0900

    article pagination template update

--- articleapp/static/articleapp/js/list.js
+++ articleapp/static/articleapp/js/list.js
@@ -8,27 +8,45 @@ function getCookie(name) {
 function initialize() {
     axios({
         method: 'get',
-        url: '/articles/list/',
+        url: '/articles/',
         headers: {
             Authorization: decodeURIComponent(getCookie('drf_token')),
         }
-//        넣어도 되고 안 넣어도 됨
     })
         .then(function (response) {
             // handle success
             console.log(response);
-
-            for (let i=0; i < response.data['results'].length; i++){
-                document.getElementById('article_list').innerHTML
-                +=
-                "<div>" +
-                "<a href=\"/articles/retrieve_template/" + response.data['results'][i]['id'] + "\">" +
-                "<img style=\"width: 100%; border-radius: 1rem;\"" +
-                " src=\"" + response.data['results'][i]['image'] +  "\" alt=\"\">" +
-                "</a>" +
-                "</div>";
+            for (let i=0; i < response.data['results'].length; i++) {
+                document.getElementById('item' + i ).innerHTML
+                    +=
+                    "<a href=\"/articles/retrieve_template/" + response.data['results'][i]['id'] + "\">" +
+                    "<img style=\"width: 100%; border-radius: 1rem;\"" +
+                    " src=\"" + response.data['results'][i]['image'] + "\"" +
+                    " alt=\"\">" +
+                    "</a>";
             }
             // paginator 기반 page 객체 생성
+            var pagination = document.getElementById('pagination');
+            if (response.data['previous'] !== null){
+            pagination.innerHTML +=
+            "<a href=\"" + response.data['previous'] + "\"" +
+            "class=\"btn btn-secondary rounded-pill\">" +
+            " Previous " +
+            "</a>";
+            }
+            pagination.innerHTML +=
+            "<a href=\"#\"" +
+            "class=\"btn btn-dark rounded-pill px-5 mx-3\">" +
+            " Current page " +
+            "</a>";
+
+            if (response.data['next'] !== null){
+            pagination.innerHTML +=
+            "<a href=\"" + response.data['next'] + "\"" +
+            "class=\"btn btn-secondary rounded-pill px-5\">" +
+            " Next " +
+            "</a>";
+            }
         })
         .catch(function (error) {
             // handle error
--- articleapp/urls.py
+++ articleapp/urls.py
@@ -1,15 +1,14 @@
 from django.urls import path
-from articleapp.views import ArticleCreateAPIView, ArticleRUDAPIView, ArticleRetrieveTemplateView, \
+from articleapp.views import ArticleRUDAPIView, ArticleRetrieveTemplateView, \
     ArticleCreateTemplateView, ArticleUpdateTemplateView, ArticleDestroyTemplateView, ArticleListTemplateView, \
-    ArticleListAPIView
+    ArticleListCreateAPIView
 
 app_name = 'articleapp'
 
 urlpatterns = [
     path('create_template/', ArticleCreateTemplateView.as_view(), name='create_template'),
-    path('list_template/', ArticleListTemplateView.as_view(), name='list_template'),
-    path('', ArticleCreateAPIView.as_view(), name='create'),
-    path('list/', ArticleListAPIView.as_view(), name='list'),
+    path('list/', ArticleListTemplateView.as_view(), name='list_template'),
+    path('', ArticleListCreateAPIView.as_view(), name='LC'),
 
     path('retrieve_template/<int:pk>', ArticleRetrieveTemplateView.as_view(), name='retrieve_template'),
     path('update_template/<int:pk>', ArticleUpdateTemplateView.as_view(), name='update_template'),
